e49aa24b7cdbec556d26b8a455de083e98079e50
Mariusz Kuśnierczyk <mar-kus@o2.pl>
2020-11-07 15:41:05 +0100

    'delete user' functionality added

---
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -0,0 +1,37 @@
+package pl.coderslab.users;
+
+import pl.coderslab.User;
+import pl.coderslab.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/users/delete")
+public class UserDelete extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+        UserDao userDao = new UserDao();
+        User delUser = userDao.read(Integer.parseInt(id));
+        request.setAttribute("id", id);
+        request.setAttribute("user", delUser);
+
+        getServletContext().getRequestDispatcher("/users/delete.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
+        UserDao userDao = new UserDao();
+        userDao.delete(Integer.parseInt(id));
+        response.sendRedirect("/users/list");
+    }
+}
